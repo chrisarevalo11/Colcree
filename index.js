1866d1d61ebee08f75d295d601def2a0f3b3d3f7
Christian Arevalo <tiancris200@gmail.com>
2022-10-05 15:36:03 -0500

    Concurso section updates

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const copyPhone = () => {
     const phoneSpan = document.getElementById("phone");
     navigator.clipboard.writeText(phoneSpan.textContent);
 
-    phoneSpan.title = "copiado!"
+    phoneSpan.title = "copiado!";
+}
+
+const textAppear = () => {
+    console.log("oe lok")
+    const texto = document.getElementById("texto-insc");
+    texto.classList.toggle("visible")
 }
